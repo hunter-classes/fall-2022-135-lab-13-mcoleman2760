69943cc18efc228870e087f33e002b509ab11d71
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2022-12-09 12:08:20 -0500

    Update funcs.cpp

--- funcs.cpp
+++ funcs.cpp
@@ -4,27 +4,21 @@
 void printRange(int left, int right){
     
     if (left > right){
-     
-               return;
-         
+        return; 
     }
 
     if (left == right){
 
         std::cout<< left << std::endl;
     }
-
-
-
     else {
+
         std::cout << left << std::endl;
          printRange(left+1,right);
-
 
     } 
     
 }
-
 
 int sumRange(int left, int right){
      
@@ -32,36 +26,29 @@ int sumRange(int left, int right){
     if (left == right){
         
         return left;
-     
     }
+
     else{
         return left + sumRange(left+1,right); 
     }
-    
-
-
-
 }
 
 
 int sumArrayInRange(int *arr, int left, int right){
-if(left > right){
+    if(left > right){
 
-return 0;
+        return 0;
 
-}
+    }
 
 
 else{
 
-return (arr[left] + sumArrayInRange(arr, left + 1, right));
+    return (arr[left] + sumArrayInRange(arr, left + 1, right));
 
 }
 
 }
-
-
-
 
 
 int sumArray(int *arr, int size){
