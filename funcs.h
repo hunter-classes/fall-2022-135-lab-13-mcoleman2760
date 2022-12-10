283f8ca547c69fa7d2fa39946fa979c0d0728cd9
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2022-12-10 16:05:01 -0500

    Update funcs.h

--- funcs.h
+++ funcs.h
@@ -1,7 +1,8 @@
 #pragma once
 #include <iostream>
 
-void printRange(int left, int right);
+std::string printRange(int left, int right);
 int sumRange(int left, int right);
 int sumArray(int *arr, int size);
 bool isAlphanumeric(std::string s);
+bool nestedParens(std::string s);
